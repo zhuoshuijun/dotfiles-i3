33299579984269fe9463e9ddf954d5633ddfeec0
zhuoshuijun <zhuoshuijun@hotmail.com>
2023-07-23 23:18:12 +0800

    update: chagne settings

--- i3blocks/scripts/i3lock.sh
+++ i3blocks/scripts/i3lock.sh
@@ -6,19 +6,17 @@ DEFAULT='#00897bE6'
 TEXT='#00897bE6'
 WRONG='#8B0000'
 VERIFYING='#00564dE6'
+RING="#006400E6"
 
 i3lock \
 --insidever-color=$CLEAR     \
 --ringver-color=$VERIFYING   \
-\
 --insidewrong-color=$CLEAR   \
 --ringwrong-color=$WRONG     \
-\
 --inside-color=$BLANK        \
---ring-color=$DEFAULT        \
+--ring-color=$RING           \
 --line-color=$BLANK          \
 --separator-color=$DEFAULT   \
-\
 --verif-color=$TEXT          \
 --wrong-color=$TEXT          \
 --time-color=$TEXT           \
@@ -26,11 +24,10 @@ i3lock \
 --layout-color=$TEXT         \
 --keyhl-color=$WRONG         \
 --bshl-color=$WRONG          \
-\
 --screen 1                   \
 --blur 5                     \
 --clock                      \
 --indicator                  \
 --time-str="%H:%M:%S"        \
---date-str="%A, %Y-%m-%d"       \
---keylayout 1 					\
+--date-str="%A, %Y-%m-%d"    \
+--keylayout 1                \
